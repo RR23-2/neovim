c65b67a6cc88858eea21e6a1455633dd9031c6e1
lc093 <ryan.rafael@binus.ac.id>
2024-08-09 18:27:18 +0700

    Added vue3 support

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -13,6 +13,7 @@ return {
 					"lua_ls",
 					"tsserver",
 					"ast_grep",
+          "volar"
 				},
 			})
 		end,
@@ -32,6 +33,10 @@ return {
 			lspconfig.ast_grep.setup({
 				capabilities = capabilities
 			})
+      lspconfig.volar.setup({
+        capabilities = capabilities,
+        filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'},
+      })
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
