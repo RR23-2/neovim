e7891b5624acb29a114cb5a5195f332b599a6b80
lc093 <ryan.rafael@binus.ac.id>
2024-08-12 08:30:09 +0700

    Adjusted catppuccin to have transparent WinSeparator (finally) and added autopairs & comment

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,11 +4,11 @@ local keymap = vim.keymap
 
 -- editing
 keymap.set("i", "jk", "<Esc>")
-keymap.set("i", "<C-h>", "<C-w>")       -- backspace 1 word
-keymap.set("i", "<C-Del>", "<C-o>dw")   -- delete 1 word
-keymap.set("v", "<leader>y", '"+y')     -- copy in visual mode to clipboard
-keymap.set("v", "<C-a>", "<Esc>ggVG")   -- select all text in file
-keymap.set("i", "<C-z>", "<Esc>ui")     -- undo in insert mode
+keymap.set("i", "<C-h>", "<C-w>") -- backspace 1 word
+keymap.set("i", "<C-Del>", "<C-o>dw") -- delete 1 word
+keymap.set("v", "<leader>y", '"+y') -- copy in visual mode to clipboard
+keymap.set("v", "<C-a>", "<Esc>ggVG") -- select all text in file
+keymap.set("i", "<C-z>", "<Esc>ui") -- undo in insert mode
 keymap.set("i", "<C-y>", "<Esc><C-r>i") -- redo in insert mode
 
 -- splitting
@@ -30,13 +30,13 @@ keymap.set({ "n", "v" }, "<C-h>", "b") -- navigate left one word
 keymap.set({ "n", "v" }, "<C-l>", "w") -- navigate left one word
 
 -- tabbing
-keymap.set("n", "<leader>to", ":tabnew<CR>")   -- open tab
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open tab
 keymap.set("n", "<leader>tc", ":tabclose<CR>") -- close tab
-keymap.set("n", "<leader>tn", ":tabn<CR>")     -- next tab
-keymap.set("n", "<leader>tp", ":tabp<CR>")     -- prev tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") -- next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") -- prev tab
 
 -- terminal controls
-keymap.set("t", "jk", [[<C-\><C-n>]])     -- exit of terminal mode
+keymap.set("t", "jk", [[<C-\><C-n>]]) -- exit of terminal mode
 keymap.set("n", "<leader>ct", ":bd!<CR>") -- close terminal
 
 -- scrolling
